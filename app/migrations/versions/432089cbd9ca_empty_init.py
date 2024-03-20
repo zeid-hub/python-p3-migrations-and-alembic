a199f28c96ef67c1aeaa462b67e6fe834d4a585d
zeid <zeidhassan4@gmail.com>
2024-03-20 23:18:31 +0300

    Done

---
+++ app/migrations/versions/432089cbd9ca_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 432089cbd9ca
+Revises: 
+Create Date: 2024-03-20 22:58:55.599875
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '432089cbd9ca'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/models.py
@@ -0,0 +1,33 @@
+# models.py
+from datetime import datetime
+
+from sqlalchemy import create_engine, desc
+from sqlalchemy import (CheckConstraint, UniqueConstraint,
+    Column, DateTime, Integer, String)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///migrations_test.db')
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+    __table_args__ = (
+        UniqueConstraint('email',
+            name='unique_email'),
+        CheckConstraint('grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12')
+    )
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
